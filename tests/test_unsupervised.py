Kmeans Heatmap, #43, rotated x-axis labels; fixed bug in TEST where I was center-scaling twice, didn't matter for test but I used tests as examples

tests/test_unsupervised.py
@@ -25,7 +25,6 @@ class UnsupervisedTests(TimerTestCase):
             raise NotImplementedError()
 
         model_fitter = ModelFitter(model=ClusteringKMeans(),
-                                   model_transformations=[CenterScaleTransformer()],
                                    fit_callback=fit_callback)
 
         # should raise an error from the callback definition above
@@ -41,7 +40,6 @@ class UnsupervisedTests(TimerTestCase):
         assert os.path.isdir(cache_directory) is False
 
         model_fitter = ModelFitter(model=ClusteringKMeans(),
-                                   model_transformations=[CenterScaleTransformer()],
                                    persistence_manager=LocalCacheManager(cache_directory=cache_directory))
         clusters = model_fitter.fit_predict(data=data, hyper_params=ClusteringKMeansHP())
 
@@ -53,7 +51,6 @@ class UnsupervisedTests(TimerTestCase):
         # clusters (i.e. commenting out `persistence_manager=...` was verified to produce different
         # `new_clusters`
         new_model_fitter = ModelFitter(model=ClusteringKMeans(seed=123),
-                                       model_transformations=[CenterScaleTransformer()],
                                        persistence_manager=LocalCacheManager(cache_directory=cache_directory))
         new_clusters = new_model_fitter.fit_predict(data=data, hyper_params=ClusteringKMeansHP())
         # noinspection PyTypeChecker
@@ -95,63 +92,54 @@ class UnsupervisedTests(TimerTestCase):
     def test_KMeans_shuffle(self):
         data = TestHelper.get_iris_data()
 
-        # determine scaling
         cluster_data = data.drop(columns='species')
-        transformations = [CenterScaleTransformer()]
-        fitter = ModelFitter(model=ClusteringKMeans(),
-                             model_transformations=transformations)
+        fitter = ModelFitter(model=ClusteringKMeans())
         clusters = fitter.fit_predict(data=cluster_data, hyper_params=ClusteringKMeansHP(num_clusters=3))
 
-        assert isclose(fitter.model.score, -140.0260445198753)
+        assert isclose(fitter.model.score, -78.94084142614594)
 
-        center_scale = CenterScaleTransformer()
-        transformed_data = center_scale.fit_transform(data_x=data)
         num_cached = fitter.model.data_x_trained_head.shape[0]
-        assert all(transformed_data.drop(columns='species').iloc[0:num_cached] == fitter.model.data_x_trained_head)  # noqa
+        assert all(data.drop(columns='species').iloc[0:num_cached] == fitter.model.data_x_trained_head)  # noqa
 
-        # 0 == virginica
-        # 1 == setosa
-        # 2 == versicolor
-        lookup = ['virginica', 'setosa', 'versicolor']
+        # setosa == 0
+        # versicolor == 1
+        # virginica == 2
+        lookup = ['setosa', 'versicolor', 'virginica']
         predicted_clusters = [lookup[x] for x in clusters]
 
         # noinspection PyTypeChecker
         confusion_matrix = ConfusionMatrix(actual_classes=data['species'].values,
                                            predicted_classes=predicted_clusters)
         assert all(confusion_matrix.matrix.setosa.values == [50, 0, 0, 50])
-        assert all(confusion_matrix.matrix.versicolor.values == [0, 39, 14, 53])
-        assert all(confusion_matrix.matrix.virginica.values == [0, 11, 36, 47])
+        assert all(confusion_matrix.matrix.versicolor.values == [0, 48, 14, 62])
+        assert all(confusion_matrix.matrix.virginica.values == [0, 2, 36, 38])
+        assert all(confusion_matrix.matrix.Total.values == [50, 50, 50, 150])
 
     def test_KMeans_no_shuffle(self):
         data = TestHelper.get_iris_data()
 
-        # determine scaling
         cluster_data = data.drop(columns='species')
-        transformations = [CenterScaleTransformer()]
-        fitter = ModelFitter(model=ClusteringKMeans(shuffle_data=False),
-                             model_transformations=transformations)
+        fitter = ModelFitter(model=ClusteringKMeans(shuffle_data=False))
         clusters = fitter.fit_predict(data=cluster_data, hyper_params=ClusteringKMeansHP(num_clusters=3))
-        assert isclose(fitter.model.score, -140.0260445198753)
 
-        # make sure the data that was trained was as expected
-        center_scale = CenterScaleTransformer()
-        transformed_data = center_scale.fit_transform(data_x=data)
+        assert isclose(fitter.model.score, -78.94084142614594)
+
         num_cached = fitter.model.data_x_trained_head.shape[0]
-        assert all(transformed_data.drop(columns='species').iloc[
-                   0:num_cached] == fitter.model.data_x_trained_head)  # noqa
+        assert all(data.drop(columns='species').iloc[0:num_cached] == fitter.model.data_x_trained_head)  # noqa
 
-        # 0 == virginica
-        # 1 == setosa
-        # 2 == versicolor
-        lookup = ['virginica', 'setosa', 'versicolor']
+        # setosa == 1
+        # versicolor == 0
+        # virginica == 2
+        lookup = ['versicolor', 'setosa', 'virginica']
         predicted_clusters = [lookup[x] for x in clusters]
 
         # noinspection PyTypeChecker
         confusion_matrix = ConfusionMatrix(actual_classes=data['species'].values,
                                            predicted_classes=predicted_clusters)
         assert all(confusion_matrix.matrix.setosa.values == [50, 0, 0, 50])
-        assert all(confusion_matrix.matrix.versicolor.values == [0, 39, 14, 53])
-        assert all(confusion_matrix.matrix.virginica.values == [0, 11, 36, 47])
+        assert all(confusion_matrix.matrix.versicolor.values == [0, 48, 14, 62])
+        assert all(confusion_matrix.matrix.virginica.values == [0, 2, 36, 38])
+        assert all(confusion_matrix.matrix.Total.values == [50, 50, 50, 150])
 
     def test_KMeans_elbow(self):
         data = TestHelper.get_iris_data()
@@ -160,31 +148,29 @@ class UnsupervisedTests(TimerTestCase):
 
         TestHelper.check_plot('data/test_unsupervised/test_kmeans_elbow_plot.png',
                               lambda: Clustering.kmeans_elbow_plot(data=data.drop(columns='species'),
-                                                                   num_clusters=num_clusters,
-                                                                   transformations=[CenterScaleTransformer()]))  # noqa
+                                                                   num_clusters=num_clusters))
 
     def test_KMeans_heatmap(self):
         data = TestHelper.get_iris_data()
         # remove setosa, in order to test 2 clusters with same amount of data, so we can verify axis on graph
-        data = data.drop(index=[0, 1, 2])
+        num_drop = 12
+        data = data.drop(index=list(range(num_drop)))
 
-        data.loc[3, 'sepal_length'] = np.nan
-        data.loc[4, 'petal_length'] = np.nan
+        data.loc[num_drop, 'sepal_length'] = np.nan
+        data.loc[num_drop+1, 'petal_length'] = np.nan
 
         assert data.isnull().sum().sum() == 2
 
         # impute missing values by species, then center/scale, then remove the species column
         transformations = [ImputationTransformer(group_by_column='species'),
-                           CenterScaleTransformer(),
                            RemoveColumnsTransformer(columns=['species'])]
 
-        fitter = ModelFitter(model=ClusteringKMeans(),
+        fitter = ModelFitter(model=ClusteringKMeans(seed=199),
                              model_transformations=transformations)
         clusters = fitter.fit_predict(data=data, hyper_params=ClusteringKMeansHP(num_clusters=3))
-
         assert data.isnull().sum().sum() == 2  # make sure original data wasn't transformed
 
-        assert all(np.bincount(np.array(clusters)) == [47, 53, 47])  # make sure 2 clusters are same size
+        assert all(np.bincount(np.array(clusters)) == [38, 38, 62])  # make sure 2 clusters are same size
         assert len(clusters) == len(data)
         # CENTER/SCALE
         TestHelper.check_plot('data/test_unsupervised/test_KMeans_heatmap_centerscale_strategy_mean.png',