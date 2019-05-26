1e3ca1bc14d335d4455da1715a5b12e9a5ecedde
Shane Kercheval <shane.kercheval@gmail.com>
2019-05-25 19:02:32 -0700

    Added additional unit test for Regression BayesianHyperOpt with Transformations

--- tests/test_BayesianOptimization.py
+++ tests/test_BayesianOptimization.py
@@ -636,3 +636,150 @@ class BayesianOptimizationTests(TimerTestCase):
                                                               predicted_values=holdout_predictions)
 
         assert score_value == 0.7905138339920948
+
+    def test_BayesianHyperOptModelTuner_Transformations_Regression_CostFunction(self):
+        data = TestHelper.get_cement_data()
+        target_variable = 'strength'
+        transformations = [RemoveColumnsTransformer(columns=['fineagg'])]
+
+        splitter = RegressionStratifiedDataSplitter(holdout_ratio=0.2)
+        training_indexes, holdout_indexes = splitter.split(target_values=data[target_variable])
+
+        training_y = data.iloc[training_indexes][target_variable]
+        training_x = data.iloc[training_indexes].drop(columns=target_variable)
+
+        holdout_y = data.iloc[holdout_indexes][target_variable]
+        holdout_x = data.iloc[holdout_indexes].drop(columns=target_variable)
+
+        score_list = [RmseScore()]
+        decorator = ModelDecorator()
+        # define & configure the Resampler object
+        resampler = RepeatedCrossValidationResampler(
+            model=ElasticNetRegressor(),  # we'll use a Random Forest model
+            transformations=transformations,
+            scores=score_list,
+            folds=5,  # 5 folds with 5 repeats
+            repeats=3,
+            fold_decorators=[decorator],
+            parallelization_cores=0)  # adds parallelization (per repeat)
+
+        from hyperopt import hp
+        space_lgb = {
+            # passing None will get removed, but work around is to pass a Transformer that doesn't do anything
+            # i.e. EmptyTransformer
+            'CenterScale vs Normalize': hp.choice('CenterScale vs Normalize vs None',
+                                                  [EmptyTransformer(),
+                                                   CenterScaleTransformer(),
+                                                   NormalizationTransformer()]),
+            'PCA': hp.choice('PCA',
+                             [EmptyTransformer(),
+                              PCATransformer()]),
+            'alpha': hp.uniform('alpha', 0.001, 2),
+            'l1_ratio': hp.uniform('l1_ratio', 0, 1),
+        }
+        max_evaluations = 5
+        model_tuner = BayesianHyperOptModelTuner(resampler=resampler,
+                                                 hyper_param_object=ElasticNetRegressorHP(),
+                                                 space=space_lgb,
+                                                 max_evaluations=max_evaluations,
+                                                 seed=6)
+        model_tuner.tune(data_x=training_x, data_y=training_y)
+
+        expected_best_transformations = {'CenterScale vs Normalize': 'EmptyTransformer',
+                                         'PCA': 'EmptyTransformer'}
+        expected_best_params = {'alpha': 1.6544370627160525, 'l1_ratio': 0.5364332695946842}
+
+        assert model_tuner.results.best_hyper_params == {**expected_best_transformations,
+                                                         **expected_best_params}
+        assert model_tuner.results.best_index == 1
+        assert all(model_tuner.results.sorted_best_indexes == [1, 0, 2, 3, 4])
+        assert model_tuner.results.number_of_cycles == max_evaluations
+        assert model_tuner.results.hyper_param_names == ['CenterScale vs Normalize', 'PCA', 'alpha', 'l1_ratio']  # noqa
+        assert model_tuner.results.transformation_names == ['CenterScale vs Normalize', 'PCA']
+        assert model_tuner.results.model_hyper_param_names == list(expected_best_params.keys())
+        assert model_tuner.results.best_model_hyper_params == expected_best_params
+
+        assert len(model_tuner.results.best_transformations) == 2
+        assert isinstance(model_tuner.results.best_transformations[0], EmptyTransformer)
+        assert isinstance(model_tuner.results.best_transformations[1], EmptyTransformer)
+
+        # should have cloned the decorator each time, so it should not have been used
+        assert len(decorator._model_list) == 0
+        # should be same number of sets/lists of decorators as there are tuning cycles
+        assert len(model_tuner.resampler_decorators) == model_tuner.results.number_of_cycles
+        assert max_evaluations == model_tuner.results.number_of_cycles
+        # only 1 decorator object passed in
+        assert all(np.array([len(x) for x in model_tuner.resampler_decorators]) == 1)
+
+        for index in range(len(model_tuner.resampler_decorators)):
+            # index = 0
+            model_list = model_tuner.resampler_decorators[index][0]._model_list
+            assert len(model_list) == 5 * 3  # 5 fold 3 repeat
+            trained_params = [x.model_object.get_params() for x in model_list]
+            # every underlying training params of the resample should be the same (a Resampler object only
+            # uses one combination of hyper-params)
+            for y in range(len(trained_params)):
+                assert trained_params[y] == trained_params[0]
+
+            # check that the hyper params that are passed in match the hyper params that the underlying
+            # model actually trains with
+            trained_params = trained_params[0]  # already verified that all the list items are the same
+            hyper_params = model_tuner.results._tune_results_objects.resampler_object.values[index].hyper_params.params_dict  # noqa
+            TestHelper.assert_hyper_params_match_2(subset=hyper_params, superset=trained_params)
+
+        TestHelper.save_string(model_tuner.results,
+                               'data/test_Tuners/test_BayesianHyperOptModelTuner_Transformations_Regression_CostFunction_results.txt')  # noqa
+
+        TestHelper.check_plot('data/test_Tuners/test_BayesianHyperOptModelTuner_Transformations_Regression_CostFunction__plot_iteration_mean_scores.png',  # noqa
+                               lambda: model_tuner.results.plot_iteration_mean_scores())
+
+        assert model_tuner.results.hyper_param_names == list(space_lgb.keys())
+        assert model_tuner.results.resampled_stats.shape[0] == max_evaluations
+
+        df = model_tuner.results.resampled_stats
+        assert model_tuner.results.best_hyper_params == \
+               df.loc[df['RMSE_mean'].idxmin(), model_tuner.results.hyper_param_names].to_dict()
+
+        ######################################################################################################
+        # Resample with best_params and see if we get the same loss value i.e. RMSE_mean
+        ######################################################################################################
+        transformations = [RemoveColumnsTransformer(columns=['fineagg'])]
+        score_list = [RmseScore()]
+
+        # define & configure the Resampler object
+        resampler = RepeatedCrossValidationResampler(
+            model=ElasticNetRegressor(),  # we'll use a Random Forest model
+            transformations=transformations,
+            scores=score_list,
+            folds=5,  # 5 folds with 5 repeats
+            repeats=3,
+            parallelization_cores=0)  # adds parallelization (per repeat)
+
+        hp = ElasticNetRegressorHP()
+        hp.update_dict(model_tuner.results.best_model_hyper_params)
+        resampler.append_transformations(model_tuner.results.best_transformations)
+        resampler.resample(training_x, training_y, hyper_params=hp)
+
+        assert OOLearningHelpers.dict_is_subset(subset=model_tuner.results.best_model_hyper_params,
+                                                superset=resampler.results.hyper_params.params_dict)
+        assert resampler.results.score_means['RMSE'] == \
+               model_tuner.results.resampled_stats.iloc[model_tuner.results.best_index]['RMSE_mean']
+
+        # Params found by Bayesian Optimization
+        transformations = [RemoveColumnsTransformer(columns=['fineagg'])]
+        pipeline = TransformerPipeline(transformations=transformations +
+                                                       model_tuner.results.best_transformations)
+        transformed_training_data = pipeline.fit_transform(training_x)
+        transformed_holdout_data = pipeline.transform(holdout_x)
+
+        model = ElasticNetRegressor()
+        hyper_params = ElasticNetRegressorHP()
+        hyper_params.update_dict(model_tuner.results.best_model_hyper_params)
+        model.train(data_x=transformed_training_data, data_y=training_y,
+                    hyper_params=hyper_params)
+        holdout_predictions = model.predict(data_x=transformed_holdout_data)
+        TestHelper.save_string(model,
+                               'data/test_Tuners/test_BayesianHyperOptModelTuner_Transformations_Regression_CostFunction_final_model_string.txt')  # noqa
+
+        score_value = RmseScore().calculate(actual_values=holdout_y, predicted_values=holdout_predictions)
+        assert score_value == 10.000002422021112
